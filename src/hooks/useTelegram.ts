294023689cdee6ef37a2c4b2df44ad3ab7361a8a
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-10 18:53:30 +0530

    Update useTelegram.ts

--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -1,1 +1,26 @@
-#
+const useTelegram = () => {
+  const tg = (window as any).Telegram?.WebApp;
+
+  const close = () => {
+    tg?.close();
+  };
+
+  const expand = () => {
+    tg?.expand();
+  };
+
+  const sendData = (data: any) => {
+    tg?.sendData?.(JSON.stringify(data));
+  };
+
+  return {
+    tg,
+    user: tg?.initDataUnsafe?.user,
+    close,
+    expand,
+    sendData,
+    isReady: Boolean(tg),
+  };
+};
+
+export default useTelegram;
