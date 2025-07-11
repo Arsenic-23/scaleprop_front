795c3fe9eeaf5fc88a86afdbe695f73bf7ea194a
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-11 21:11:43 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,23 @@
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        background: 'gradientBG 10s ease infinite',
+      },
+      keyframes: {
+        gradientBG: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
+      backgroundSize: {
+        '200': '200% 200%',
+      },
+    },
   },
   plugins: [],
 };
