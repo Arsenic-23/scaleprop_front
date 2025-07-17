82f53a04e47773b22bfff46ac3cd75732487abc2
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-17 11:40:03 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,16 @@ export default {
     extend: {
       animation: {
         background: 'gradientBG 10s ease infinite',
+        blink: 'blink 1s step-start infinite',
       },
       keyframes: {
         gradientBG: {
           '0%, 100%': { backgroundPosition: '0% 50%' },
           '50%': { backgroundPosition: '100% 50%' },
+        },
+        blink: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
         },
       },
       backgroundSize: {
